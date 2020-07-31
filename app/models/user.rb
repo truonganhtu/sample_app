78fda35d6c92d1b0577aac0addaf32bd8b23261b
Anh Tu <tatltpt@gmail.com>
2020-07-31 16:17:17 +0700

    Chap6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = Settings.user.email_regex
+
+  validates :name, presence: true,
+    length: {maximum: Settings.user.username_maximum}
+  validates :email, presence: true,
+    length: {maximum: Settings.user.email_maximum},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true
+  validates :password, presence: true,
+    length: {minimum: Settings.user.password_minimum}
+  
+  has_secure_password
+  
+  before_save :email_downcase
+
+  private  
+
+  def email_downcase
+    self.email = email.downcase
+  end
 end
