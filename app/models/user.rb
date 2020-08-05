731b1287ccc77e2e1f0d31999e941900b424828f
Anh Tu <tatltpt@gmail.com>
2020-08-05 13:45:22 +0700

    Chapter9

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.user.email_regex
   USERS_PARAMS = %i(name email password password_confirmation).freeze
 
+  attr_accessor :remember_token
+
   validates :name, presence: true,
     length: {maximum: Settings.user.username_maximum}
   validates :email, presence: true,
@@ -14,6 +16,36 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save :email_downcase
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
 
   private
 
